7e11d0264da6c68ee65e02141a3173c499d12a08
Hakim <horairyhakim@gmail.com>
2025-12-08 12:30:03 +0100

    feature: Including memory to the LLM model

--- main.py
+++ main.py
@@ -2,38 +2,57 @@ from groq import Groq
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
 
-def read_file(file_path):
-    with open(file_path , "r") as file:
-        return file.read()
+class ConversationAgent:
+    def __init__(self):
+        load_dotenv()
+        self.client = Groq(api_key=os.environ["GROQ_KEY"])
+        self.initiate_history()
 
 
-def ask_llm(user_interaction):
+    @staticmethod
+    def read_file(file_path):
+        with open(file_path , "r") as file:
+            return file.read()
 
-    client = Groq(api_key=os.environ["GROQ_KEY"])
 
-    chat_completion = client.chat.completions.create(
-        messages=[
-
+    def initiate_history(self):
+        self.history = [
             {
                 "role": "system",
-                "content": read_file("./context.txt")
-            },
+                "content": ConversationAgent.read_file("./context.txt")
+            }]
 
-            {
-                "role": "user",
-                "content": user_interaction,
-            }
-        ],
 
-        model="llama-3.3-70b-versatile"
-    )
 
-    return chat_completion.choices[0].message.content
+    def ask_llm(self, user_interaction):
+
+        self.history.append(
+                {
+                    "role": "user",
+                    "content": user_interaction,
+                })
+
+        response = self.client.chat.completions.create(
+            messages=self.history,
+            model="llama-3.3-70b-versatile"
+        ).choices[0].message.content
+        
+        self.history.append(
+                {
+                    "role": "assistant",
+                    "content": response,
+                })
+
+
+        return response
 
 
 
 if __name__ == "__main__":
-    response = ask_llm(user_interaction="Explique le thérome central limite")
-    print(response)
+    conversation_agent = ConversationAgent()
+    conversation_agent.ask_llm(user_interaction="Quel âge as-tu ?")
+    print(conversation_agent.history)
+    print('-----')
+    conversation_agent.ask_llm(user_interaction="Quelle était ma première question ?")
+    print(conversation_agent.history)
